2f436d850e7cc6cc55ed31d8c0b844eab116b86c
Leonardo Palluzzi <lp.palluzzi@gmail.com>
2025-05-08 00:12:36 +0200

    fix index controller

--- controllers/productController.js
+++ controllers/productController.js
@@ -3,39 +3,53 @@ const connection = require('../db/db')
 
 function index(req, res) {
 
+
     const page = Number(req.query.page) || 1
     const limit = Number(req.query.limit) || 5
     const offset = (page - 1) * limit
 
     const productSql = 'SELECT * FROM products LIMIT ? OFFSET ?'
+    const imagesSql = 'SELECT * FROM images WHERE images.product_id = ?'
 
     connection.query(productSql, [limit, offset], (err, products) => {
         if (err) return res.status(500).json({ state: 'error', message: err.message });
-        const productsList = products
+        const productList = products
 
-        const productListToSend = productsList.map(product => {
-            return itemToSend = {
-                slug: product.slug,
-                name: product.name,
-                price: product.price,
-                description: product.description,
-                created_at: product.created_at,
-                item_number: product.item_number,
-                quantity: product.quantity,
-            }
-        })
+        const productListToSend = (productList.map(product => {
+            return new Promise((resolve, reject) => {
+                connection.query(imagesSql, [product.id], (err, images) => {
+                    if (err) return res.status(500).json({ state: 'error', message: err.message });
 
-        res.json(productListToSend)
-    })
+                    const itemToSend = {
+                        slug: product.slug,
+                        name: product.name,
+                        price: product.price,
+                        description: product.description,
+                        created_at: product.created_at,
+                        item_number: product.item_number,
+                        quantity: product.quantity,
+                        images: images
+                    }
+                    resolve(itemToSend)
+                })
+            })
+        }))
+
+        console.log(productListToSend);
+
+        Promise.all(productListToSend)
+            .then(productListToSend => {
+                res.json(productListToSend)
+            })
+            .catch(err => res.status(500).json({ state: 'error', message: err.message }))
+    });
 }
 
 function show(req, res) {
 
-    const imagesSql = 'SELECT * FROM images WHERE images.product_id = ?'
-    const transactionsSql = 'SELECT * FROM transactions WHERE transactions.product_id = ?'
-    const categorySql = 'SELECT * FROM categories WHERE categories.id = ?'
-    const licenseSql = 'SELECT * FROM licenses WHERE licenses.id = ?'
-    const promotionSql = 'SELECT * FROM promotions WHERE promotions.id = ?'
+    const slug = req.params.slug
+
+    const productSql = 'SELECT * FROM products WHERE products.slug = ?'
 
 }
 
