983daabf3972c47f8861e02948542970d5cac63a
Leonardo Palluzzi <lp.palluzzi@gmail.com>
2025-05-07 17:40:06 +0200

    Add environment configuration and database connection setup

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 const cors = require('cors');
+const productRouter = require('./routers/productRouter')
+const connection = require('./db/db')
+
+app.use(cors({
+    origin: 'http://localhost:5173'
+}))
 
 app.use(express.json())
+
+app.use(express.static('/public'))
+
+// app.use('/api/v1/funkboolpop', productRouter)
 
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
